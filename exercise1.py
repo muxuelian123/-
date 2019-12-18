5ab795fc57f691f96bc59fe19ec2381832947450
lv <12346@qq.com>
2019-12-18 15:02:33 +0800

    message

---
+++ exercise1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+print('hello world   阿伦.艾弗森')
+print('hello world   贾森.威廉姆斯')
+print('hello world   科比.布莱恩特')
+print('hello world   沙克.奥尼尔')
+print('hello world   卡梅隆.安东尼')
+print('hello world   勒布朗.詹姆斯')
+print('hello world   德维恩.韦德')
+print('hello world   德克.诺维斯基')
+print('hello world   卡哇伊.莱昂纳德')
+print('hello world   保罗.乔治')
+print('hello world   拉塞尔.威斯布鲁克')
+
+
